09d90a2e61225a9a11c12665264f1ccf895adbe0
kerami <keramiozsoy@hotmail.com>
2019-04-14 21:51:19 +0300

    dockerfile

---
+++ shop/container/docker/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "********************************************************"
+echo "Starting Shop Application"
+echo "********************************************************"
+java -jar /app/shop-0.0.1-SNAPSHOT.jar
